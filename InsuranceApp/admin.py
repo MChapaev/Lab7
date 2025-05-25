ef80a72805f39bf7e97263e23433692bd8ef8a46
MChapaev <magomed.chapaev1@mail.ru>
2025-05-25 22:21:36 +0300

    Add admin access to database

--- InsuranceApp/admin.py
+++ InsuranceApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Client, Policy, Claim
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'passport_number', 'birth_date', 'phone', 'email')
+    list_filter = ('birth_date',)
+    search_fields = ('full_name', 'passport_number')
+    ordering = ('full_name',)
+
+@admin.register(Policy)
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'policy_type', 'start_date', 'end_date', 'premium')
+    list_filter = ('start_date', 'end_date', 'policy_type')
+    search_fields = ('policy_type', 'client__full_name')
+    ordering = ('start_date',)
+
+@admin.register(Claim)
+class ClaimAdmin(admin.ModelAdmin):
+    list_display = ('id', 'policy', 'claim_date', 'amount', 'description')
+    list_filter = ('claim_date', 'policy__policy_type')
+    search_fields = ('description', 'policy__policy_type')
+    ordering = ('claim_date',)
